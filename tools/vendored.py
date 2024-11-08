7e02a4ab55515bd748d467f2e86a062d17d38ec7
Stanislav Levin <slev@altlinux.org>
2024-11-08 11:23:23 +0300

    vendor: update tomli to 2.0.2
    tomli 2.0.1 => 2.0.2 carries several fixes:
    https://github.com/hukkin/tomli/blob/master/CHANGELOG.md#202
    
    Fixes: https://github.com/stanislavlevin/pyproject_installer/issues/82
    Signed-off-by: Stanislav Levin <slev@altlinux.org>

--- tools/vendored.py
+++ tools/vendored.py
@@ -9,7 +9,7 @@ Currently there are no changes made on vendored packages.
 
 To verify changes if any:
 - tomli: https://github.com/hukkin/tomli.git
-git diff 2.0.1:src/tomli @:src/pyproject_installer/_vendor/tomli
+git diff 2.0.2:src/tomli @:src/pyproject_installer/_vendor/tomli
 - packaging: https://github.com/pypa/packaging
 git diff 24.1:src/packaging @:src/pyproject_installer/_vendor/packaging
 """
