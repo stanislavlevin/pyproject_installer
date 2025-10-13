23adcc8aaa56454c3c25c1c044ba4e0854fba68c
Stanislav Levin <slev@altlinux.org>
2025-10-13 17:28:10 +0300

    vendor: update packaging to 25.0
    Update vendored packaging from 24.2 to 25.0,
    which brought fixes and improvements:
    https://packaging.pypa.io/en/stable/changelog.html#changelog
    
    Fixes: https://github.com/stanislavlevin/pyproject_installer/issues/102
    Signed-off-by: Stanislav Levin <slev@altlinux.org>

--- tools/vendored.py
+++ tools/vendored.py
@@ -11,7 +11,7 @@ To verify changes if any:
 - tomli: https://github.com/hukkin/tomli.git
 git diff 2.1.0:src/tomli @:src/pyproject_installer/_vendor/tomli
 - packaging: https://github.com/pypa/packaging
-git diff 24.2:src/packaging @:src/pyproject_installer/_vendor/packaging
+git diff 25.0:src/packaging @:src/pyproject_installer/_vendor/packaging
 """
 
 from pathlib import Path
